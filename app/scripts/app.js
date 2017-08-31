a446d4b5d2bc72db8be00d2f8d4b9bfc107439ad
Gabe Tiller <gabectiller@gmail.com>
2017-08-31 17:30:02 -0400

    fixed syntax error

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 
 (function() {
+
      function config($stateProvider, $locationProvider) {
        $locationProvider
          .html5Mode({
@@ -7,21 +8,22 @@
              requireBase: false
          });
 
-         $stateProvider
-           .state('landing', {
-               url: '/',
-               templateUrl: '/templates/landing.html'
-             })
-           .state('album', {
-               url: '/album',
-               templateUrl: '/templates/album.html'
-             })
-            .state('collection', {
-                url: '/collection',
-                templateUrl: '/templates/collection.html'
-             });
+       $stateProvider
+         .state('landing', {
+           url: '/',
+           templateUrl: '/templates/landing.html'
+         })
+         .state('album', {
+           url: '/album',
+           templateUrl: '/templates/album.html'
+         })
+         .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+         });
+      }
 
-             angular
-            .module('blocJams', ['ui.router'])
-            .config(config);
-    })();
+      angular
+        .module('blocJams', ['ui.router'] )
+        .config(config);
+})();
