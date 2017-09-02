23efbeeaa5f41f3416d5bac7ced85537c35c4ddd
Gabe Tiller <gabectiller@gmail.com>
2017-09-02 12:47:36 -0400

    updating master before checkpoint

--- app/scripts/controllers/Landingctrl.js
+++ app/scripts/controllers/Landingctrl.js
@@ -1,5 +1,5 @@
 
-//Defining a controller for the landing view. This is what will glue the actualy view to the template.
+//Defining a controller for the landing view. This is what will glue the actualy view to the template. A controller is a JavaScript object created by a constructor function. Controllers contain the "business logic" that apply functions and values to the scope
 
 (function() {
      function LandingCtrl() {
