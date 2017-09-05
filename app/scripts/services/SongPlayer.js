3ef0444aa7021f8776faebefdc9f7870d33c2087
Gabe Tiller <gabectiller@gmail.com>
2017-09-05 12:59:42 -0400

    added documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,35 +3,39 @@
           var SongPlayer = {};
 
           var currentSong = null;
-                  /**
-        * @desc Buzz object audio file
-        * @type {Object}
-        */
+/**
+* @desc Buzz object audio file
+* @type {Object}
+*/
           var currentBuzzObject = null;
 
-            /**
-   * @function setSong
-   * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-   * @param {Object} song
-   */
 
+/**
+* @function playSong
+* @desc Plays the current Buzz object. Sets the playing property of the song object to true
+* @param {Object} song
+*/
    var playSong = function(song) {
          currentBuzzObject.play();
          song.playing = true;
    }
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/
     var setSong = function(song) {
            if (currentBuzzObject) {
                currentBuzzObject.stop();
                currentSong.playing = null;
            }
-   /**
-   * @function playSong
-   * @desc Plays the current Buzz object. Sets the playing property of the song object to true
-   * @param {Object} song
-   */
 
 
 
+/**
+* @desc Buzz object audio file
+* @type {Object}
+*/
          currentBuzzObject = new buzz.sound(song.audioUrl, {
              formats: ['mp3'],
              preload: true
@@ -39,7 +43,15 @@
 
          currentSong = song;
       };
-
+/**
+* @function SongPlayer.play
+* @desc If the currently playing song is not the same as the song the user clicks on, then we want to:
+        1.Stop the currently playing song, if there is one.
+        2.Set a new Buzz sound object.
+        3.Set the newly chosen song object as the currentSong.
+        4..Play the new Buzz sound object.
+* @param {Object} song
+*/
      SongPlayer.play = function(song) {
        if (currentSong !== song) {
 
@@ -53,7 +65,11 @@
            }
        }
      };
-
+/**
+* @function SongPlayer.play
+* @desc If the user clicks on the currentBuzzObject (current song) we pause the song and set the song.play boolean to false.
+* @param {Object} song
+*/
      SongPlayer.pause = function(song) {
      currentBuzzObject.pause();
      song.playing = false;
