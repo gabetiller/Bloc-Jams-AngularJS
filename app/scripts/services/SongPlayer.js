caf25d8d2628181e11e5736e397d3e13b0a7497e
Gabe Tiller <gabectiller@gmail.com>
2017-09-05 15:38:46 -0400

    assignment-8 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -13,15 +13,7 @@
           var currentBuzzObject = null;
 
 
-/**
-* @function playSong
-* @desc Plays the current Buzz object. Sets the playing property of the song object to true
-* @param {Object} song
-*/
-   var playSong = function(song) {
-         currentBuzzObject.play();
-         song.playing = true;
-   }
+
 /**
 * @function setSong
 * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -57,11 +49,33 @@
       var getSongIndex = function(song) {
        return currentAlbum.songs.indexOf(song);
 };
+
+/**
+* @function playSong
+* @desc Plays the current Buzz object. Sets the playing property of the song object to true
+* @param {Object} song
+*/
+   var playSong = function(song) {
+         currentBuzzObject.play();
+         song.playing = true;
+   }
+
+/**
+* @function stopSong
+* @desc Stops the current Buzz object. Sets the playing property of the song object to true
+* @param {Object} song
+*/
+  var stopSong = function(song) {
+        currentBuzzObject.stop();
+        song.playing = null;
+  }
+
 /**
 * @desc Active song object from list of songs
 * @type {Object}
 */
     SongPlayer.currentSong = null;
+
 /**
 * @function SongPlayer.play
 * @desc If the currently playing song is not the same as the song the user clicks on, then we want to:
@@ -71,20 +85,23 @@
         4..Play the new Buzz sound object.
 * @param {Object} song
 */
+
      SongPlayer.play = function(song) {
        song = song || SongPlayer.currentSong;
+       // if we click on a new song
        if (SongPlayer.currentSong !== song) {
 
          setSong(song);
 
          playSong(song);
-
-         } else if (SongPlayer.currentSong === song) {
-           if (currentBuzzObject.isPaused()) {
-               currentBuzzObject.play();
-           }
+      // if we click on the current song or click the player bar play button
+       } else if (SongPlayer.currentSong === song) {
+         if (currentBuzzObject.isPaused()) {
+             playSong(song);
+         }
        }
      };
+
 /**
 * @function SongPlayer.previous
 * @desc gets the previous index of the song. If the song is the first song and the user clicks previous, the current song stops. if it's not the first song, it plays the index.
@@ -95,14 +112,32 @@
      currentSongIndex--;
 
      if (currentSongIndex < 0) {
-         currentBuzzObject.stop();
-         SongPlayer.currentSong.playing = null;
+         stopSong();
        } else {
            var song = currentAlbum.songs[currentSongIndex];
            setSong(song);
            playSong(song);
        }
  };
+
+ /**
+ * @function SongPlayer.next
+ * @desc gets the next index of the song. If the song is the last song and the user clicks previous, the current song stops. if it's not the first song, it plays the index.
+ * @param {Object} Songplayer.currentSong
+ */
+
+     SongPlayer.next = function() {
+     var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+     currentSongIndex++;
+     if (currentSongIndex > currentAlbum.songs) {
+         stopSong();
+       } else {
+           var song = currentAlbum.songs[currentSongIndex];
+           setSong(song);
+           playSong(song);
+       }
+
+};
 /**
 * @function SongPlayer.play
 * @desc If the user clicks on the currentBuzzObject (current song) we pause the song and set the song.play boolean to false.
